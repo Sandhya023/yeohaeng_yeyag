8a42c360a6342a677bff128078d5e56856f3c345
Sandhya Yadav <yadavshndhya@gmail.com>
2020-11-19 17:20:48 +0530

    onboarding

--- App.js
+++ App.js
@@ -8,107 +8,110 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
+  Image,
+  TouchableOpacity,
   Text,
-  StatusBar,
 } from 'react-native';
+import { NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Onboarding, DestinationDetail } from './screens/'
+import { COLORS, icons, SIZES } from './constants';
+import Tabs from './navigation/tabs';
 
-const App: () => React$Node = () => {
+const theme = {
+  ...DefaultTheme,
+  colors: {
+      ...DefaultTheme.colors,
+      border: "transparent",
+  },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator initialRouteName={'Onboarding'}>
+      <Stack.Screen
+                    name="Onboarding"
+                    component={Onboarding}
+                    options={{
+                        title: null,
+                        headerStyle: {
+                            backgroundColor: COLORS.white
+                        },
+                        headerLeft: null,
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Pressed")}
+                            >
+                                <Image
+                                    source={icons.barMenu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                />
+    
+      <Stack.Screen name="Home"
+        component={Tabs}
+        options={{
+          title: null,
+          headerStyle: {
+            backgroundColor: COLORS.white
+          },
+          headerLeft: ({ onPress }) => (
+            <TouchableOpacity
+                                style={{ marginLeft: SIZES.padding }}
+                                onPress={onPress}
+                            >
+                                <Image
+                                    source={icons.back}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity style={{marginRight: SIZES.padding}}
+            onPress={() => console.log("Menu")}>
+               <Image
+                  source={icons.menu}
+                  resizeMode="contain"
+                  style={{
+                      width: 25,
+                      height: 25,
+                  }}
+              />
+
+            </TouchableOpacity>
+          ),
+        }} 
+        />
+      </Stack.Navigator>
+
+      
+    </NavigationContainer>
+      
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
-export default App;
+export default () => {
+  return <App />;
+};
---
+++ screens/DestinationDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DestinationDetail = () => {
+    return (
+        <View>
+            <Text>Onboading</Text>
+        </View>
+    )
+}
+export default DestinationDetail;
